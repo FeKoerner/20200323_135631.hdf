5504e8ea1ba7a8eec07229a90054666c53650b2b
pi3 <pi3@berlin>
2022-01-17 15:42:47 +0100

    speedtest result upload

---
+++ speedtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# test internet speed over time
+
+# check credentials
+eval $(ssh-agent)
+ssh-add /home/pi/.ssh/id_github
+sleep 10
+# file for results
+CSV=speed_results.csv
+
+speedtest --csv-header >> $CSV
+while true; do
+    speedtest --csv >> $CSV
+    git add .
+    git commit -m "speedtest result upload"
+    git push
+    sleep 5m
+done
